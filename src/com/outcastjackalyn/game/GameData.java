949ffb38a39d981c7f717e7d7bf3fe71968369d7
jackalyn m <kaos@thegame.com>
2018-04-21 15:58:33 +0100

    gamedata beginnings

--- src/com/outcastjackalyn/game/GameData.java
+++ src/com/outcastjackalyn/game/GameData.java
@@ -1,5 +1,19 @@
 package com.outcastjackalyn.game;
 import com.outcastjackalyn.objects.player.PlayerData;
+import jjcard.text.game.impl.Item;
+import jjcard.text.game.impl.Location;
+import jjcard.text.game.impl.Mob;
+import jjcard.text.game.impl.Player;
 
 public class GameData {
+
+    private String seed = "000000";
+
+    private Mob[] mobs = {};
+    private Item[] items = {};
+    private Location[] locations = {};
+    private Player player;
+    private Location start;
+
+
 }
